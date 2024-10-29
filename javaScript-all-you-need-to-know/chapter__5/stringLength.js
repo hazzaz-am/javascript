cf8becd5092f945991ef8dc2713b7fbd88cb65a1
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-10-29 09:23:10 +0600

    completed chapter-5

---
+++ javaScript-all-you-need-to-know/chapter__5/stringLength.js
@@ -0,0 +1,13 @@
+var a = "hazzaz abdul mannan";
+let length = 0;
+
+while (true) {
+	if (a.charAt(length) === "") {
+		break;
+	}
+
+	length++;
+}
+
+console.log(length);
+console.log(a.length);
--- javaScript-all-you-need-to-know/chapter__5/stringMethods.js
+++ javaScript-all-you-need-to-know/chapter__5/stringMethods.js
@@ -40,13 +40,59 @@ console.log(a.concat(" ", b, " ", c));
 console.log(c.constructor); //  [Function: String]
 
 var nam = "hazzaz abdul mannan";
-// console.log(nam.endsWith("mannan")); // true
-// console.log(nam.endsWith("n")); // true
-// console.log(nam.endsWith("m")); // false
-// console.log(nam.endsWith("man")); // false
+console.log(nam.endsWith("mannan")); // true
+console.log(nam.endsWith("n")); // true
+console.log(nam.endsWith("m")); // false
+console.log(nam.endsWith("man")); // false
 
 var nam = "hazzaz abdul mannan ";
 // console.log(nam.endsWith(" ")); // true
 
-console.log(nam.endsWith('abdul', 13)); // search string in specific length 
+console.log(nam.endsWith("abdul", 13)); // search string in specific length
 
+console.log(nam.startsWith("h"));
+console.log(nam.startsWith("hazzaz"));
+console.log(nam.startsWith("hazzaz", 8)); // false
+console.log(nam.startsWith("hazzaz", 0)); // true
+
+var trims = "        nice";
+var trime = "      hello      ";
+console.log(trime.trimStart());
+console.log(trime.trimEnd());
+console.log(trims.trim());
+console.log(trims.toUpperCase());
+console.log(trims.toLowerCase());
+
+var hello = "Hello World!";
+console.log(hello.includes("e")); // true
+console.log(hello.includes("world")); // false
+console.log(hello.includes("World")); // true
+console.log(hello.includes("hello", 6)); // false
+
+var a = "I am learning javascript";
+console.log(a.indexOf("learning")); // 5
+console.log(a.indexOf("a")); // 2
+
+var b = "hello world";
+console.log(b.lastIndexOf("l")); // 9
+console.log(b.lastIndexOf("l", 1)); // -1
+
+let text = "The rain in SPAIN stays mainly in the plain";
+console.log(text.match("SPAIN"));
+
+var myself = "hazzaz";
+console.log(myself.repeat(4)); // hazzazhazzazhazzazhazzaz
+
+var a = "hello world";
+console.log(a.replace("l", "d"));
+console.log(a.replaceAll("l", "d"));
+
+
+var sliced = "hazzaz abdul mannan";
+console.log(sliced.slice(0, 6)); // hazzaz
+console.log(sliced.slice(-1));
+
+console.log(sliced.split(" ")); // ["hazzaz", "abdul", "mannan"]
+
+console.log(sliced.substring(2, 0));
+console.log(sliced.substring(-10)); //hazzaz abdul mannan
